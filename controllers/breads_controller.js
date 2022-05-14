5ff3894d6eeed39cd4a38091a408eb2fa910e8cd
TheLegendary1248 <1248alex+NCU@gmail.com>
2022-05-13 20:37:19 -0400

    Sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,10 +2,17 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
-// SHOW
-breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-  })  
+// INDEX
+breads.get('/', (req, res) => {
+    res.render('Index',
+      {
+        breads: Bread,
+        title: 'Index Page'
+      }
+    )
+})
+
+
 
 module.exports = breads
 
